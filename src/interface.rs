25c4f47c841ace00b5011b2ffcaffd3364d08c88
PeterQLee <leep1995@gmail.com>
2019-12-12 20:23:55 -0500

    add default 1.0 kvalue mode

--- src/interface.rs
+++ src/interface.rs
@@ -1,10 +1,9 @@
-
 use crate::parse::{NoiseField, SwathElem};
 use crate::read_from_archive::{get_data_from_zip_path, SentinelArchiveOutput};
 use crate::apply::{apply_swath_scale, prep_measurement, restore_scale, convert_to_f64_f32, convert_to_u16_f32};
 use crate::estimate::*;
 extern crate libc;
-use ndarray::{Array1, Array2};
+use ndarray::{Array1, Array2, arr1};
 use std::ptr;
 use numpy::{PyArray, PyArray1, PyArray2};
 use pyo3::prelude::{Py, pymodule,  PyModule, PyResult, Python, PyErr};
@@ -139,7 +138,54 @@ fn denoise_engine(_py: Python, m:&PyModule) -> PyResult<()> {
         //return Err(exceptions::ValueError("bad parsing path"));
     }
 
-    
+
+    #[pyfn(m, "get_noscale_data")]
+    fn get_noscale_data(__py:Python, zippath:&str) -> PyResult<(Py<PyArray2<f64>>,Py<PyArray2<u16>>, Py<PyArray1<f64>>)> {
+        let lambda_ = &[0.1,0.1,6.75124,2.78253,10.0]; //convert to array
+        let lambda2_ = 1.0;
+        let mu = 1.7899;
+        let gamma = 2.0;
+        let zipval = get_data_from_zip_path(zippath, true);
+        match zipval {
+            Some(archout) => {
+                match archout {
+                    SentinelArchiveOutput::BothPolOutput(swath_bounds, w, mut noisefield, x16, co16) => {
+                        let mut x_:Option<Array2<f64>> = None;
+                        {
+                            let mut y = noisefield.data.view_mut();
+                            x_ = Some(prep_measurement(x16.view(), y));
+                        }
+                        let mut x = x_.unwrap();
+                        
+                        let sb:Vec<&[SwathElem]> = swath_bounds.iter().map(|a| a.as_slice()).collect();
+                        
+                        //let k = estimate_k_values(x.view(), noisefield.data.view(), &w, &sb, mu, gamma, lambda_, lambda2_);
+                        let k = arr1(&[1.0,1.0,1.0,1.0,1.0]);
+                        
+                        apply_swath_scale(x.view_mut(), noisefield.data.view(), k.view(), &sb);
+                        
+                        restore_scale(x.view_mut());
+
+
+                        let py_cross = PyArray::from_array(__py,&x).to_owned();
+                        let py_co = PyArray::from_array(__py,&co16).to_owned();
+                        let py_k = PyArray::from_array(__py, &k).to_owned();
+                        return Ok((py_cross, py_co, py_k));
+
+                    },
+                    _ => {}
+
+                }
+                
+            },
+            _ => {}
+        }
+
+        return exceptions::ValueError.into();
+        //return Err(exceptions::ValueError("bad parsing path"));
+    }
+
+    m.add_wrapped(wrap_pyfunction!(get_noscale_data))?;
     m.add_wrapped(wrap_pyfunction!(get_dualpol_data))?;
     Ok(())
 }
